16c51e85dd4ad20a13034f38b584602e0b037f6f
Igor Shpak <Iamverybyakya@gmail.com>
2019-09-26 09:47:24 +0300

    Lab1

---
+++ BCIT/LAB1/Program.cs
@@ -0,0 +1,166 @@
+
+using System;
+
+using static System.Math;
+namespace BCIT
+{
+    class BIEQ
+    {
+
+        static string[] ReadArgs()
+        {
+
+            string[] Coef;
+            Coef = new string[3];
+            int a;
+            int i = 0;
+            while (i < 3)
+            {
+                System.Console.WriteLine($"Введите  {i+1}  значение");
+                Coef[i] = System.Console.ReadLine();
+                if (Int32.TryParse(Coef[i], out a))
+                {
+                    i++;
+                }
+                else
+                {
+                    Console.ForegroundColor=ConsoleColor.Red;
+                    Console.WriteLine("Неверный формат ввода");
+                    Console.ResetColor();
+                }
+            }
+            return (Coef);
+        }
+        static double[] NumberOfRoots(int[] ICoef)
+        {
+            System.Console.WriteLine($"{Pow(ICoef[1],2)} {ICoef[0]} {ICoef[2]}");
+            double D = (Pow(ICoef[1],2) - 4 * ICoef[0] * ICoef[2]);
+            System.Console.WriteLine(D);
+            int Nroots = 0;
+            double[] Roots;
+            if (ICoef[0] == 0)
+            {
+                if (ICoef[1]==0)
+                {
+                    if (ICoef[2] == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Корни любые");
+                        Console.ResetColor();
+                        return (null);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Корней нет");
+                        Console.ResetColor();
+                        return (null);
+                    }
+                }
+                if (ICoef[2]/ICoef[1]<0)
+                {
+                    Console.WriteLine(ICoef[2] / ICoef[1]);
+                    Nroots = 2;
+                    Roots = new double[2];
+                    Roots[0] = Sqrt(-ICoef[2] / ICoef[1]);
+                    Roots[1] = -Sqrt(-ICoef[2] / ICoef[1]);
+                    for (int i = 0; i < Nroots; i++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"x {i} = {Roots[i]}");
+                        Console.ResetColor();
+                    }
+                    return (Roots);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    System.Console.WriteLine("Корней нет");
+                    Console.ResetColor();
+                    return (null);
+                }
+            }
+            if (D > 0)
+            {
+                if (((-ICoef[1] - Sqrt(D)) / (2 * ICoef[0]) > 0) && ((-ICoef[0] + Sqrt(D)) / (2 * ICoef[0]) > 0))
+                {
+                    Console.WriteLine("Часть 1");
+                    Nroots = 4;
+                    Roots = new double[4];
+                    Roots[0] = Sqrt((-ICoef[1] - Sqrt(D)) / (2 * ICoef[0]));
+                    Roots[1] = -Sqrt((-ICoef[1] - Sqrt(D)) / (2 * ICoef[0])) ;
+
+                    Roots[2] = Sqrt((-ICoef[1] + Sqrt(D)) / (2 * ICoef[0]));
+                    Roots[3] = -Sqrt((-ICoef[1] + Sqrt(D)) / (2 * ICoef[0]));
+                    for (int i = 0; i < Nroots; i++)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"x {i} = {Roots[i]}");
+                        Console.ResetColor();
+                    }
+                    return (Roots);
+                }
+                else if (((-ICoef[1] - Sqrt(D)) / (2 * ICoef[0]) > 0) || ((-ICoef[1] + Sqrt(D)) / (2 * ICoef[0]) > 0))
+                {
+                    Console.WriteLine("Часть 2");
+                    Nroots = 2;
+                    Roots = new double[2];
+                    if (((-ICoef[0] - D) / (2 * ICoef[0])) > 0)
+                    {
+                        D = (-ICoef[1] - Sqrt(D)) / (2 * ICoef[0]);
+                        Roots[0] = Sqrt(D);
+                        Roots[1] = -Sqrt(D);
+                        for (int i = 0; i < Nroots; i++)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"x {i} = {Roots[i]}");
+                            Console.ResetColor();
+                        }
+                        return (Roots);
+                    }
+                    else if (((-ICoef[1] + Sqrt(D)) / (2 * ICoef[0])) > 0)
+                    {
+                        Console.WriteLine("Часть 3");
+                        D = ((-ICoef[1] + Sqrt(D)) / (2 * ICoef[0]));
+                        Roots[0] = Sqrt(D);
+                        Roots[1] = -Sqrt(D);
+                        for (int i = 0; i < Nroots; i++)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine($"x {i} = {Roots[i]}");
+                            Console.ResetColor();
+                        }
+                        return (Roots);
+                    }
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Корней нет");
+            Console.ResetColor();
+            return null;
+        }
+        static int[] ConvertArgs(string[] Sroots)
+        {
+            int[] ICoef = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                ICoef[i] = Int32.Parse(Sroots[i]);
+            }
+            return (ICoef);
+        }
+        static void Main(string[] args)
+        {
+
+            int[] ICoef = new int[3];
+            //args = ReadArgs();
+            try { ICoef = ConvertArgs(args); }
+            catch (Exception)
+            {
+                ICoef = ConvertArgs(ReadArgs());
+            }
+            double[] Nroots = NumberOfRoots(ICoef);
+            Console.WriteLine("AAAAAAAAAAAA");
+            Console.ReadKey();
+        }
+    }
+}
