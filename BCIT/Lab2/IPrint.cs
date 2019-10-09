cfd3d3aa6be2c27239a09a31686e4da0aea2b109
Igor Shpak <Iamverybyakya@gmail.com>
2019-10-09 13:15:06 +0300

    Lab 1-3

---
+++ BCIT/Lab2/IPrint.cs
@@ -0,0 +1,7 @@
+﻿namespace Figures
+{
+    interface IPrint
+    {
+        void Print();
+    }
+}
---
+++ BCIT/Lab2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Math;
+namespace Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Шпак Игорь ИУ5-34";
+            Rectangle rect = new Rectangle(100, 200);
+            Square square = new Square(53);
+            Circle circle = new Circle(PI);
+
+            rect.Print();
+            square.Print();
+            circle.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
